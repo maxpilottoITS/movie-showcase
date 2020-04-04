dee4c8dfadc74fdad9539b75f92ce3726827cf75
maxpilotto <maxpil99@outlook.com>
2020-04-04 19:36:24 +0200

    Add landscape detailed view

--- app/src/main/java/com/maxpilotto/movieshowcase/activities/DetailActivity.java
+++ app/src/main/java/com/maxpilotto/movieshowcase/activities/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.maxpilotto.movieshowcase.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 import com.maxpilotto.movieshowcase.App;
 import com.maxpilotto.movieshowcase.R;
 import com.maxpilotto.movieshowcase.models.Movie;
@@ -19,24 +21,25 @@ import com.maxpilotto.movieshowcase.services.DataProvider;
 import java.util.Calendar;
 
 public class DetailActivity extends AppCompatActivity {
-    private ImageView backdrop;
     private TextView title;
     private TextView overview;
     private TextView userRating;
     private TextView personalRating;
     private TextView year;
+    private Integer orientation;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        backdrop = findViewById(R.id.backdrop);
         title = findViewById(R.id.title);
         overview = findViewById(R.id.overview);
         userRating = findViewById(R.id.userRating);
         personalRating = findViewById(R.id.personalRating);
         year = findViewById(R.id.year);
+
+        orientation = getResources().getConfiguration().orientation;
 
         loadContent();
 
@@ -63,17 +66,23 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void loadContent() {
+        RequestManager glide = Glide.with(this);
         Integer id = getIntent().getIntExtra(MainActivity.ID_EXTRA, 0);
         Movie movie = Database.get().getLocalMovie(id);     //TODO All db calls should be async
 
         title.setText(movie.getTitle());
         overview.setText(movie.getOverview());
-        userRating.setText(getString(R.string.userRating,movie.getVoteAverage()));
-        personalRating.setText(getString(R.string.yourRating,movie.getRating()));
-        year.setText(getString(R.string.year,movie.getYear()));
+        userRating.setText(getString(R.string.userRating, movie.getVoteAverage()));
+        personalRating.setText(getString(R.string.yourRating, movie.getRating()));
+        year.setText(getString(R.string.year, movie.getYear()));
 
-        Glide.with(this)
-                .load(movie.getCoverPath())
-                .into(backdrop);
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            glide.load(movie.getCoverPath())
+                    .into((ImageView) findViewById(R.id.backdrop));
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            glide.load(movie.getPosterPath())
+                    .into((ImageView) findViewById(R.id.poster));
+        }
     }
 }
