7cc25cfd9410eb2c500ebf3823f595175c998a17
maxpilotto <maxpil99@outlook.com>
2020-05-19 20:51:42 +0200

    Improve endless scroll

--- app/src/main/java/com/maxpilotto/movieshowcase/services/DataProvider.java
+++ app/src/main/java/com/maxpilotto/movieshowcase/services/DataProvider.java
@@ -5,14 +5,19 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 
+import com.maxpilotto.kon.JsonArray;
+import com.maxpilotto.kon.JsonObject;
 import com.maxpilotto.kon.net.JsonService;
 import com.maxpilotto.movieshowcase.App;
 import com.maxpilotto.movieshowcase.R;
 import com.maxpilotto.movieshowcase.models.Movie;
 import com.maxpilotto.movieshowcase.models.MovieDecoder;
+import com.maxpilotto.movieshowcase.persistance.Database;
 import com.maxpilotto.movieshowcase.persistance.MovieProvider;
 import com.maxpilotto.movieshowcase.protocols.AsyncTaskSimpleCallback;
 import com.maxpilotto.movieshowcase.protocols.MovieUpdateCallback;
@@ -23,12 +28,11 @@ import java.util.List;
 
 import static com.maxpilotto.movieshowcase.util.Util.asyncTask;
 
-/**
- * Data provider for this application
- */
 public final class DataProvider {
     private static DataProvider instance;
     private ConnectivityManager connectivityManager;
+    private Integer totalResults = 0;
+    private Integer totalPages = 0;
 
     public static void init(Context context) {
         instance = new DataProvider();
@@ -59,22 +63,27 @@ public final class DataProvider {
                 if (!requestNewData) {
                     Log.d(App.TAG, "No new data was requested, won't look for updates");
                 } else if (!hasInternet()) {
+//                    movies = Movie.parseList(
+//                            contentResolver.query(MovieProvider.URI_MOVIES,null,null,null,null)
+//                    );
+
                     Log.d(App.TAG, "Not connected, won't look for updates");
                 } else {
-                    List<Movie> remoteMovies = JsonService
-                            .fetchObject(Routes.discover(page))
-                            .getObjectList("results", MovieDecoder::decode);
+                    JsonObject json = JsonService.fetchObject(Routes.discover(page));
+                    List<Movie> remoteMovies = json.getObjectList("results", MovieDecoder::decode);
+
+                    totalPages = json.getInt("total_pages");
+                    totalResults = json.getInt("total_results");
 
                     for (Movie m : remoteMovies) {
                         contentResolver.insert(MovieProvider.URI_MOVIES, m.values());
+
+                        Log.d(App.TAG, "Downloaded: " + m);
                     }
+
+                    movies = Database.get().getLocalMovies((page - 1) * getResultsPerPage(),getResultsPerPage());
                 }
 
-                Cursor cursor = contentResolver.query(MovieProvider.URI_MOVIES, null, null, null, null);
-
-                movies = Movie.parseList(cursor);
-
-                cursor.close();
 
                 Log.d(App.TAG, "Loaded records: " + movies.size());
             }
@@ -84,5 +93,9 @@ public final class DataProvider {
                 callback.onLoad(movies);
             }
         });
+    }
+
+    public Integer getResultsPerPage() {
+        return totalResults != 0 && totalPages != 0 ? totalResults / totalPages : 20;
     }
 }
