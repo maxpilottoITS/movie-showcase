e9905e62ba4d585e39dc438db93bbe34cfb7b76a
maxpilotto <maxpil99@outlook.com>
2020-06-24 10:47:42 +0200

    Add search route

--- app/src/main/java/com/maxpilotto/movieshowcase/util/Routes.java
+++ app/src/main/java/com/maxpilotto/movieshowcase/util/Routes.java
@@ -1,12 +1,24 @@
 package com.maxpilotto.movieshowcase.util;
 
 public final class Routes {
-    public static final String DISCOVER = "https://api.themoviedb.org/3/discover/movie";
+    public static final String BASE = "https://api.themoviedb.org/3/";
+    public static final String DISCOVER = BASE + "discover/movie";
+    public static final String SEARCH = BASE + "search/movie";
     public static String API_KEY = "";
 
     public static String discover(Integer page) {
-        return "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY +
+        return DISCOVER +
+                "?api_key=" + API_KEY +
                 "&page=" + page;
+    }
+
+    public static String search(String query, String language, boolean includeAdult, int year) {
+        return SEARCH +
+                "?api_key=" + API_KEY +
+                "&language=" + language +
+                "&query=" + query +
+                "&include_adult=" + includeAdult +
+                "&year=" + year;
     }
 
     private Routes() {
